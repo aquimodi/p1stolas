1774a1680a79e91890bef2b893b0c6fc21a6d5f7
aquimodi <aquilestudent@gmail.com>
2025-06-13 13:43:25 +0200

    Deleted test-cors.js

--- server/data/sampleData.js
+++
@@ -1,7 +0,0 @@
-// Sample data for the backend - emptied for production
-import { generateUniqueId } from '../utils/helpers.js';
-
-// Empty sample data arrays for production
-export const sampleProjects = [];
-export const sampleIncidents = [];
-export const sampleUsers = [];
--- src/data/sampleData.ts
+++
@@ -1,8 +0,0 @@
-import { Project, Order, DeliveryNote, Equipment, EstimatedEquipment, Incident, IncidentComment } from '../types';
-import { generateUniqueId } from '../utils/helpers';
-
-// Empty sample data for production
-export const sampleProjects: Project[] = [];
-
-// Empty sample incidents for production
-export const sampleIncidents: Incident[] = [];
